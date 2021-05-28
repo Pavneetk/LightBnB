97b074e4606b297b17ad55e000bf87f0fc5231f8
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-28 00:35:44 +0000

    Convert get reservations into sql query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -76,7 +76,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+    `, [guest_id, limit])
+    .then((result) => {
+      console.log(result);
+      return Promise.resolve(result.rows);
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
@@ -92,11 +110,10 @@ exports.getAllReservations = getAllReservations;
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      return result.rows;
-      //console.log(result.rows);
+      return Promise.resolve(result.rows);
     })
     .catch((err) => {
-      console.log(err.message);
+      return Promise.reject(err);
     });
 };
 exports.getAllProperties = getAllProperties;
